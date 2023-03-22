ef1234b745903b59fd576f5a67793e57cb70348f
Lillian Oquendo <LillianOquendo1@gmail.com>
2023-03-22 14:59:32 -0400

    First Class Functions Lab

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// code your solution here
+//Figure out how to permit the default activity to be overridden
+console.log (saturdayFun ());
+function saturdayFun(activity){
+if (activity) {
+    return (`This Saturday\, I want to ${activity}!`)
+}
+else {
+    return ('This Saturday\, I want to roller-skate!');
+}
+}
+
+mondayWork();
+function mondayWork (activity2){
+    if (activity2){
+        return (`This Monday\, I will ${activity2}.`)
+    }
+    else {
+        return `This Monday\, I will go to the office.`
+    }
+
+}
+let wrapAdjective = function(type = "*") {
+	return function(word = "special") {
+		return `You are ${type}${word}${type}!`
+		}
+	}
+    
+    
+
+
+
+
